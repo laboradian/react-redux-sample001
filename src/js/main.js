06476e381afe7de7af6df3210c033ee38f4c0082
kura5 <laboradian@gmail.com>
2017-07-13 14:38:19 +0900

    Add some samples

--- src/js/main.js
+++ src/js/main.js
@@ -6,9 +6,10 @@ import '../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.js';
 require('file-loader?name=../../dist/[name].[ext]!../index.html');
 
 
+import _ from 'lodash'
 import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
+import { Provider, connect } from 'react-redux'
 import { createStore } from 'redux'
 import todoApp from './reducers'
 import App from './components/App'
@@ -24,6 +25,176 @@ render(
   </Provider>,
   document.getElementById('root-todolist')
 );
+
+// ----------------------------
+
+(() => {
+
+
+  const additionAction = (value) => {
+    return {
+      type: 'ADD_VALUE',
+      value
+    }
+  }
+  const clearAdditionAction = () => {
+    return {
+      type: 'CLEAR_VALUE',
+    }
+  }
+
+  const additionReducer = (state = 0, action) => {
+    switch (action.type) {
+      case 'ADD_VALUE':
+        return state + action.value
+      case 'CLEAR_VALUE':
+        return 0;
+      default:
+        return state
+    }
+  }
+
+  const AdditionComponent = ({ total, onClickToAdd, onClickToClear }) => {
+    let textInput;
+    return (
+      <div>
+          <label>足し算</label>
+          <input type="text"
+              id="exampleInputName1"
+              placeholder="10"
+              defaultValue="100"
+              ref={(input) => { textInput = input; }}
+          />
+          <button onClick={() => onClickToAdd(textInput.value) }>足す</button>
+          <button onClick={onClickToClear}>クリア</button>
+          合計 <span>{total}</span>
+      </div>
+    );
+  };
+
+  AdditionComponent.propTypes = {
+    total: PropTypes.number,
+  };
+
+  const mapStateToProps = (state/*, ownProps*/) => {
+    const props = {
+      total: state
+    };
+    return props;
+  }
+
+  const mapDispatchToProps = (dispatch) => {
+    return {
+      onClickToAdd: (value) => {
+        const val = parseInt(value);
+        if (_.isNaN(val)) {
+          //
+        } else {
+          dispatch(additionAction(val));
+        }
+      },
+      onClickToClear: () => {
+        dispatch(clearAdditionAction());
+      }
+    }
+  }
+  
+  const AdditionContainer = connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(AdditionComponent)
+
+
+  const store = createStore(additionReducer)
+
+  render(
+    <Provider store={store}>
+      <AdditionContainer />
+    </Provider>,
+    document.getElementById('s12')
+  )
+
+})();
+
+// ----------------------------
+
+(() => {
+
+  const Welcome = function(props) {
+    return <h3>Hello, {props.name}</h3>;
+  };
+
+  //const element = <Welcome name="Foo" />;
+
+  const Avatar = function(props) {
+    return (
+      <img className="Avatar"
+        src={props.user.avatarUrl}
+        alt={props.user.name}
+      />
+    );
+  };
+
+  const UserInfo = function(props) {
+    return (
+      <div className="UserInfo">
+        <Avatar user={props.user} />
+        <div className="UserInfo-name">
+          {props.user.name}
+        </div>
+      </div>
+    );
+  };
+
+  const Comment = function(props) {
+    return (
+      <div className="Comment">
+        <UserInfo user={props.author} />
+        <div className="Comment-text">
+          {props.text}
+        </div>
+        <div className="Comment-date">
+          {formatDate(props.date)}
+        </div>
+      </div>
+    );
+  };
+
+  function formatDate(date) {
+    return date.toLocaleDateString();
+  }
+
+  const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+      name: 'Hello Kitty',
+      avatarUrl: 'http://placekitten.com/g/64/64'
+    }
+  };
+
+  const App = function() {
+    return (
+      <div>
+        <Welcome name="Foo" />
+        <Welcome name="Bar" />
+        <Welcome name="Baz" />
+        <Comment
+          author={comment.author}
+          text={comment.text}
+          date={comment.date}
+        />
+      </div>
+    );
+  };
+
+  render(
+    <App />,
+    document.getElementById('s11')
+  );
+
+})();
+
 
 // ----------------------------
 
